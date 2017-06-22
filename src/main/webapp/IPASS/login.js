1187646f6553fdca845bb7ff8417ae55aca136d4
PC-DOUWE\Douwe <douweschepers@icloud.com>
2017-06-22 08:31:09 +0200

    linkkk

--- src/main/webapp/IPASS/login.js
+++ src/main/webapp/IPASS/login.js
@@ -1,5 +1,5 @@
 function login(){
-	$.getJSON("http://localhost:4711/firstapp/restservices/medewerker/", function(data) {
+	$.getJSON("src/main/java/nl/hu/v1wac/firstapp/webservices/restservices/medewerker/", function(data) {
 		$(data).each(function( index , value ) {
 			var username = $('#username').val();
 			var password = $('#password').val();
