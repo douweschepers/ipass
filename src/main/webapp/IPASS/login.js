28d391e7d8de7156cb866c2478458a62a6606b13
PC-DOUWE\Douwe <douweschepers@icloud.com>
2017-06-22 08:21:52 +0200

    https link

--- src/main/webapp/IPASS/login.js
+++ src/main/webapp/IPASS/login.js
@@ -1,5 +1,5 @@
 function login(){
-	$.getJSON("restservices/medewerker", function(data) {
+	$.getJSON("http://localhost:4711/firstapp/restservices/medewerker/", function(data) {
 		$(data).each(function( index , value ) {
 			var username = $('#username').val();
 			var password = $('#password').val();
@@ -18,56 +18,4 @@ function login(){
 }
 		
 		
-	/*var data = $("#loginform").serialize();
-	console.log(data);
-	var uri="restservices/medewerker/authentication";
-	 $.ajax(uri, {
-		 type: "post",
-		 data: data,
-		 success: function() {
-			 console.log("ingelogd");
-		 },
-		 error: function() {
-			 console.log("error");
-		 }
-	 });
-		
-		$("#login").on("click", function() {
-			var data = $("#loginform").serialize();
-			console.log(data);
-			$.post("../restservices/authentication", data, function(response) {
-			window.sessionStorage.setItem("sessionToken", response);
-			$.ajaxSetup({
-				beforeSend: function(xhr){
-			 var token = window.sessionStorage.getItem("sessionToken");
-			 xhr.setRequestHeader( 'Authorization', 'Bearer ' + token);
-				}
-			})
-			$.getJSON("http://localhost:4711/firstapp/restservices/medewerker/", function(data) {
-			$(data).each(function( index , value ) {
-				
-				var username = $('#username').val();
-				var password = $('#password').val();
-				if(username==value.gebruikersnaam){
-					if(password ==value.wachtwoord){
-						localStorage.setItem("medewerkers_id", value.medewerkers_id);
-						localStorage.setItem("rol", value.rol);
-						window.open("persoonsgegevens.html","_self");
-					}
-					else{
-						alert("Gebruikersnaam/wachtwoord komen niet overeen!");
-					}
-				}
-			});
-		});
-			
-			
-			window.open("persoonsgegevens.html","_self");
-			}).fail(function(jqXHR, textStatus, errorThrown) {
-			console.log(textStatus);
-			console.log(errorThrown);
-			});
-
 	
-		});*/
-
